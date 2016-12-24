8355c3a6e1bfccc03ff1a458c7b082930cb5740b
jvonessen <cgajen09@gmail.com>
2016-12-23 17:50:10 -0800

    clean version

--- script.js
+++ script.js
@@ -4,23 +4,3 @@ $(document).ready(function(){
     $('.hero img').attr('src', imgSrc);
   });
 })
-
-//     ".hero".empty();
-//     this.addClass("hero");
-
-
-//     $("img.top").replaceWith(this);
-
-
-//     $(".hero").removeClass("hero");
-//     $(".top").remove();
-//     $(this).addClass("hero");
-//     $(this).removeClass("thumbnail");
-
-
-// $((".hero").find("img").attr('src')).replaceWith($(this).find("img").attr('src'));
-
-
-//     var topLink = $(".hero").find("img").attr("src");
-//     var newLink = $(this).find("img").attr("src");
-//     $((".hero").find("img").attr("src")).replaceWith(newLink);
